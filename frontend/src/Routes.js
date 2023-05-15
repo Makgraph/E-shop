3f7e23a6dd29c9963ed438f97c73b6195b2225b8
Makgraph <milienmakenson3@gmail.com>
2023-05-15 16:48:37 -0400

    first commit

---
+++ frontend/src/Routes.js
@@ -0,0 +1,5 @@
+import LoginPage from "../src/pages/Login";
+import SignupPage from "../src/pages/SignupPage";
+import ActivationPage from "../src/pages/ActivationPage.jsx";
+
+export { LoginPage, SignupPage, ActivationPage };
